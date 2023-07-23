e6205a643fcbc4ea2e6549e54fafd23936762a15
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-07-23 17:03:24 -0400

    estore

--- MyEstore/src/main/java/com/tms/estore/MyEstoreApplication.java
+++ MyEstore/src/main/java/com/tms/estore/MyEstoreApplication.java
@@ -2,7 +2,11 @@ package com.tms.estore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableJpaRepositories(basePackages = "com.tms.estore.repository")
+@EnableTransactionManagement
 @SpringBootApplication
 public class MyEstoreApplication {
 
