ddd67da49644a0ce5fdabeebc6e4968e4fab5696
Yauhen Barsukov <tenniszh2009@gmail.com>
2023-07-23 16:55:16 -0400

    estore

---
+++ MyEstore/src/main/java/com/tms/estore/MyEstoreApplication.java
@@ -0,0 +1,13 @@
+package com.tms.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEstoreApplication.class, args);
+    }
+
+}
---
+++ MyEstore/src/test/java/com/tms/estore/MyEstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.estore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
